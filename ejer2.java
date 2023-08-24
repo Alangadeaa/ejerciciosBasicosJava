5568f2da4edd6f131d168286040d0c432c0d1a9c
Alangadeaa <102254187+Alangadeaa@users.noreply.github.com>
2023-08-24 08:23:21 -0300

    Add files via upload

---
+++ ejer2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class ejer2 {
+
+ public static void main(String[] args) {
+   System.out.println("Ingrese numeros del 1 al 50 (ambos incluidos) separados por un espacio.");
+   Scanner scanner = new Scanner(System.in);
+   String lineaNumeros = scanner.nextLine();
+   Scanner numerosScanner = new Scanner(lineaNumeros);
+   while (numerosScanner.hasNextInt()) {
+     int noesprimo = 0;
+     int numero = numerosScanner.nextInt();
+     if (numero > 50 || numero < 0 ) {
+       System.out.println("Ingrese numeros que no sean mayores de 50 ");
+       return;
+     }
+     
+    
+       for (int x = numero - 1 ; x > 1; x--) {
+         if (numero % x == 0 ) {
+           noesprimo = 1;
+           System.out.printf("%d ", x);
+           
+         }
+       } 
+       
+
+     if(noesprimo == 0) {
+       System.out.println("Primo:" + numero);
+     } else {
+       System.out.printf("\n");
+     }
+   }
+   
+   numerosScanner.close();
+   scanner.close();
+ }
+
+}
